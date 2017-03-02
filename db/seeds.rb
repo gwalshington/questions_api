192bebd74f5cf7e2f39e6322318fc8972e311b81
Walsh Costigan <walshcostigan@gmail.com>
2017-03-02 18:21:04 -0500

    adding questions api

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(question: "Why are you studying a foreign language?")
+Question.create(question: "What are your favorite hobbies?")
+Question.create(question: "Describe yourself using three words")
+Question.create(question: "What are your favorite hobbies?")
+Question.create(question: "What is one goal you’d like to accomplish during your lifetime?")
